0148b9ad4ca33d362291ab4fad0567322b2ab778
Jaden McElvey <32574860+JadenMcElvey@users.noreply.github.com>
2021-09-24 17:36:21 -0400

    Update Example.py
    Separate and describe fin parameters in Example.py

--- Example.py
+++ Example.py
@@ -9,7 +9,10 @@ ENG_FILE = cwd + r"\Resources\TCIMC3_(9899.7771)_5_5_2.329_11.0789_2.eng"
 csvPath = cwd + "\\Temp\\"
 
 # Choose the fin parameters to simulate
-finParams = [(16,7,4,10)]
+# First tuple is the parameters for the booster
+# Second tuple is the parameters for the sustainer
+# Fin parameters are in formar (root chord, span, tip chord, sweep)
+finParams = [(16,7,4,10), (16,7,4,10)]
 # Create an object to run the simulations
 finSim = AutoRASAero()
 # Set the paths for the simulations
@@ -17,7 +20,7 @@ finSim.setPaths(CDX1_FILE, ENG_FILE, csvPath)
 # Start RASAero
 finSim.startupRASAero()
 # run the simulation and print the output
-result = finSim.runSimulation(finParams[0], finParams[0], 6)
+result = finSim.runSimulation(finParams[0], finParams[1], 6)
 print(result)
 # Close RASAero
 finSim.closeRASAero()
