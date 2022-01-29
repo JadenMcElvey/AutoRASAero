ce25818309c743b2a7d09326f9effa6fa56a927f
Jaden McElvey <jadenmcelvey@gmail.com>
2022-01-29 14:48:13 -0500

    January 2022 Update
    -Updated rocket file
    -Associated changes to file paths and optimizer

--- Example.py
+++ Example.py
@@ -4,8 +4,8 @@ from Automation.AutoRASAero import AutoRASAero
 
 # Get paths for rocket files and where to save the csv
 cwd = str(pathlib.Path.cwd())
-CDX1_FILE = cwd + r"\Resources\MBS_43.CDX1"
-ENG_FILE = cwd + r"\Resources\TCIMC3_(9899.7771)_5_5_2.329_11.0789_2.eng"
+CDX1_FILE = cwd + r"\Resources\MBS_43_A.CDX1"
+ENG_FILE = cwd + r"\Resources\PmotorRasp2.eng"
 csvPath = cwd + "\\Temp\\"
 
 # Choose the fin parameters to simulate
